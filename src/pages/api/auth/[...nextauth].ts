bbf1a346592868553dd2499661046275b60d3121
K4ME <guilhermegodencio@gmail.com>
2022-02-02 19:54:55 -0300

    Creating user using faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query as q } from "faunadb";
+
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import {fauna} from '../../../Services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +13,20 @@ export default NextAuth({
       //scope: 'read:user'
     }),
   ],
+  /* jwt: {
+    signingkey: process.env.SIGNING_KEY,
+  }, */
+  callbacks: {
+    async signIn({ user, account, profile }): Promise<boolean> {
+      const { email } = user;
+
+      try {
+        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 })
